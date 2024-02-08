49557fa03174585eb0b7a0eff65a8b387b391a51
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-02-08 19:03:12 +0000

    Modified Serializers.py Views.py and Urls.py and add missing folder

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-
+from drf_api.permissions import IsOwnerOrReadOnly
 
 class ProfileList(APIView):
     """
@@ -13,27 +13,37 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+            )
         return Response(serializer.data)
+
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
-            profile = Profile.objets.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valied():
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
